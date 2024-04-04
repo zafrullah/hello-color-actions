3abfabca8e738c38f84a0bf16064b269e4ff2107
Zaf Mohammed <zafrullah@gmail.com>
2024-04-04 16:58:19 -0400

    Initial Commit, changed hello message

--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hello World from the last frontier!";
 console.log(hello());
 exports.hello = hello;
 
